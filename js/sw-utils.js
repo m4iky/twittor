d467c936d58db385ffa9535a0b0f03f0b0140b44
m4iky <michaelvargas29@hotmail.com>
2018-10-25 10:30:03 -0400

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,21 @@
+
+
+// Guardar en el caché dinámico
+function actualizaCacheDinamico ( cacheName, req, res) {
+
+
+    if ( res.ok) {
+
+       return caches.open(cacheName).then ( cache => {
+
+            cache.put(req, res.clone());
+
+            return res.clone();
+        })
+
+
+    } else {
+        return res;
+    }
+
+}
